2286d882b35e30020f805b7f5d1636d4c95e91c6
Nita Constantin <nita.constantin880722@yahoo.com>
2022-08-18 11:32:38 +0300

    02 calling GET Post

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,6 +1,7 @@
-import { Component, Input } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Post } from '../post.model';
 import { PostsService } from '../posts.service';
+import { Subscription } from 'rxjs';
 
 @Component({
     selector: 'app-post-list',
@@ -8,16 +9,28 @@ import { PostsService } from '../posts.service';
     styleUrls: ['./post-list.component.css']
 
 })
-export class PostListComponent{
+export class PostListComponent implements OnInit, OnDestroy{
     // posts = [
     //     {title: "First Post", content: "This is the first post 's content"},
     //     {title: "Second Post", content: "This is the second post 's content"},
     //     {title: "Third Post", content: "This is the third post 's content"},
 
     // ]
-    @Input() posts:Post[] = [];
+    posts:Post[] = [];
+    private postsSub: Subscription;
 
     constructor(public postsService: PostsService ){
+    }
+    ngOnDestroy() {
+        this.postsSub.unsubscribe();
+    }
+
+    ngOnInit(){
+        this.posts = this.postsService.getPosts();
+        this.postsSub = this.postsService.getPostsUpdateListener().
+            subscribe((posts: Post[]) => {
+                this.posts = posts;
+            })
     }
 
 }
--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -1,17 +1,23 @@
 import { Injectable } from "@angular/core";
-import { Title } from "@angular/platform-browser";
+import { Subject } from 'rxjs'
 import { Post } from "./post.model";
 
 @Injectable({providedIn: 'root'})
 export class PostsService{
     private posts: Post[] = [];
+    private postsUpdated = new Subject<Post[]>()
 
     getPosts(){
         return [...this.posts];
     }
 
+    getPostsUpdateListener(){
+        return this.postsUpdated.asObservable();
+    }
+
     addPost(title: string, content: string){
         const post : Post = {title:title, content: content}
-        this.posts.push(post)
+        this.posts.push(post);
+        this.postsUpdated.next([...this.posts])
     }
 }
