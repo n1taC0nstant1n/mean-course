687d31e2f21689630d4757d12719ddcb6722b6d7
Nita Constantin <nita.constantin880722@yahoo.com>
2022-08-21 18:35:56 +0300

    04 Connecting our Node Express App to MongoDB

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const Post = require('./models/post');
-const app = express();
 
+const app = express();
+mongoose.connect('mongodb+srv://brad1234:brad1234@cluster0.9bxlps0.mongodb.net/?retryWrites=true&w=majority')
+    .then(() => {
+        console.log('Connected to database!')
+    })
+    .catch(() => {
+        console.log('Connection failed!');
+    })
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}));
 
