1e1adc4cd3bb23cea38f7b5888c5bfacb4264a27
Nita Constantin <nita.constantin880722@yahoo.com>
2022-08-22 12:41:25 +0300

    04 transforming response data

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { Subject } from 'rxjs'
 import { Post } from "./post.model";
+import { map } from 'rxjs/operators'
 
 @Injectable({providedIn: 'root'})
 export class PostsService{
@@ -13,9 +14,19 @@ export class PostsService{
     }
 
     getPosts(){
-       this.http.get<{message: string, posts:Post[]}>('http://localhost:3000/api/posts')
-       .subscribe((postData) => {
-            this.posts = postData.posts;
+       this.http
+       .get<{message: string, posts: any}>('http://localhost:3000/api/posts')
+       .pipe(map((postData) =>{
+            return postData.posts.map(post => {
+                return{
+                    title: post.title,
+                    content: post.content,
+                    id: post._id
+                }
+            })
+       }))
+       .subscribe((transformedPosts) => {
+            this.posts = transformedPosts;
             this.postsUpdated.next([...this.posts]);
        });
     }
