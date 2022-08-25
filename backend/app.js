01c97d63ce08d630965879b6899c11222f19c01e
Nita Constantin <nita.constantin880722@yahoo.com>
2022-08-25 16:32:42 +0300

    69 Re-organizing Backend Routes

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const Post = require('./models/post');
+const postsRoutes = require('./routes/posts')
 
 const app = express();
 mongoose.connect('mongodb+srv://brad1234:brad1234@cluster0.9bxlps0.mongodb.net/node-angular?retryWrites=true&w=majority')
@@ -24,63 +24,6 @@ app.use((req, res, next) => {
     next();
     });
 
-app.post('/api/posts', (req, res, next) => {
-    const post = new Post({
-        title: req.body.title,
-        content: req.body.content
-    });//req.body;
-    //console.log(post);
-    post.save().then( createdPost => {
-        res.status(201).json({
-            message: 'Post added succesfully',
-            postId: createdPost._id
-        });
-    });
-    
-});
-
-app.put('/api/posts/:id', (req, res, next) => {
-    const post = new Post({
-        _id: req.body.id,
-        title: req.body.title,
-        content: req.body.content
-    })
-    Post.updateOne({_id: req.params.id}, post).then((result) => {
-        console.log(result);
-        res.status(200).json({message:'Update successful'});
-
-    })
-})
-
-app.get('/api/posts',(req, res, next) => {
-    //next()
-   Post.find()
-    .then(documents => {
-        console.log(documents);
-        res.json({
-            message: 'Posts fetch succesfully!',
-            posts: documents
-           });
-    })
-   
-});
-
-app.get('/api/posts/:id', (req, res, next) => {
-    Post.findById(req.params.id).then(post => {
-        if(post){
-            res.status(200).json(post)
-
-        } else{
-            res.status(404).json({ message: 'Post not found'})
-        }
-    })
-});
-
-app.delete('/api/posts/:id', (req, res, next) => {
-    Post.deleteOne({_id: req.params.id}).then(result => {
-        console.log(req.params.id);
-        res.status(200).json({message: 'Post deleted!'});
-    });
-});
+app.use('/api/posts/',postsRoutes);
 
 module.exports = app;
