9eccdb5175a6f19d170c01fc7da66c64840da010
Nita Constantin <nita.constantin880722@yahoo.com>
2022-09-07 09:13:29 +0300

    98 Creating the User Model

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,9 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const postsRoutes = require('./routes/posts')
+const postsRoutes = require('./routes/posts');
+const userRoutes = require('./routes/user');
+
 
 const app = express();
 mongoose.connect('mongodb+srv://brad1234:brad1234@cluster0.9bxlps0.mongodb.net/node-angular?retryWrites=true&w=majority')
@@ -28,5 +30,7 @@ app.use((req, res, next) => {
     });
 
 app.use('/api/posts/',postsRoutes);
+app.use('/api/user/',userRoutes);
+
 
 module.exports = app;
