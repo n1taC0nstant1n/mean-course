af86b2a1125642b075490ee63e021083eb793da3
Nita Constantin <nita.constantin880722@yahoo.com>
2022-09-11 22:57:02 +0300

    115 Adding a reference to the Model

--- backend/models/post.js
+++ backend/models/post.js
@@ -5,6 +5,7 @@ const postSchema = mongoose.Schema({
     content: {type: String, required: true},
     imagePath: {type: String, required: true},
     creator: {type: mongoose.Schema.Types.ObjectId, ref: 'User', require: true }
+    
 });
 
 module.exports = mongoose.model('Post', postSchema);
